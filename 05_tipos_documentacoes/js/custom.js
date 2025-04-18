deee74c42c03b2da6ffb6c42d7ae5d1c9858ba7f
Paulo-SenacRS <prjunior@senacrs.com.br>
2025-04-17 22:06:34 -0300

    diagramação conteúdo 5

---
+++ 05_tipos_documentacoes/js/custom.js
@@ -0,0 +1,25 @@
+
+$(".painel-arquivo button").mouseleave(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-00.png)');
+})
+$(".btn-matricula").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-01.png)');
+})
+$(".btn-iptu").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-02.png)');
+})
+$(".btn-itr").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-03.png)');
+})
+$(".btn-guia").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-04.png)');
+})
+$(".btn-rg_cpf").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-05.png)');
+})
+$(".btn-registros").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-06.png)');
+})
+$(".btn-certidoes").mouseover(function() {
+    $(".painel-arquivo").css('background-image','url(img/painel-clicavel-07.png)');
+})
