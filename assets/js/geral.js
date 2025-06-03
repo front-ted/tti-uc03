70a58f23f38a344a45bffe17484b72c5ef3e1511
Paulo-SenacRS <prjunior@senacrs.com.br>
2025-06-03 15:08:01 -0300

    inserção loader antigo

--- assets/js/geral.js
+++ assets/js/geral.js
@@ -9,7 +9,7 @@ window.addEventListener('load', function(){
       return new bootstrap.Popover(popoverTriggerEl)
     })
 
-    $('.preloader').delay(2200).fadeOut(500);
+    $('.preloader').delay(1500).fadeOut(500);
 
     $('.botao').click(function(){
       $(this).addClass('visitado');
